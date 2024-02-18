5b811d8f2173ab1d77e2cfca128de06ea6a10b5d
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2024-02-18 11:15:18 +0600

    login api

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,6 +1,6 @@
 const prisma = require("../../prisma");
-const { hashPassword } = require("../helper/authHelper");
-
+const { hashPassword, comparePassword } = require("../helper/authHelper");
+const jwt = require("jsonwebtoken")
 const handleRegister = async (req, res) => {
     try {
       const { name, email, password, phone, address } = req.body;
@@ -43,6 +43,64 @@ const handleRegister = async (req, res) => {
         message: error.message,
       });
     }
+};
+
+const handleLogin = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      if (!email || !password) {
+        return res.status(400).json({
+          message: "Email and password are required",
+        });
+      }
+  
+      const user = await prisma.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
+  
+      if (!user) {
+        return res.status(404).json({
+          message: "Email not found",
+        });
+      }
+  
+      const passwordMatch = await comparePassword(password, user.password);
+  
+      if (!passwordMatch) {
+        return res.status(401).json({
+          message: "Incorrect email or password",
+        });
+      }
+  
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+  
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+          maxAge: 7 * 24 * 60 * 60 * 1000,
+          sameSite: "none",
+        })
+        .json({
+          success: true,
+          message: "Login successful",
+          user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            address: user.address,
+            phone: user.phone,
+            isAdmin: user.isAdmin,
+          },
+        });
+    } catch (error) {
+      return res.status(500).json({
+        message: error.message,
+      });
+    }
   };
 
-module.exports = {handleRegister}
+module.exports = {handleRegister, handleLogin}
